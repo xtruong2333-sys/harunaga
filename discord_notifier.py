d30238a9c8394b38d8685b3f6bfa499749b439e2
xtruong2333-sys <xtruong2333@gmail.com>
2026-09-12 17:21:16 +0700

    feat(discord): Set 3D pig & caterpillar avatar URLs

--- discord_notifier.py
+++ discord_notifier.py
@@ -112,13 +112,13 @@ def send_discord_alert(webhook_url: str, video_info: dict, channel_name: str, th
     # Tên định danh & Ảnh đại diện riêng cho từng loại Bot trên Discord
     if alert_type == "outlier" or outlier_score >= 3.0:
         bot_username = "chạy đâu con sâu"
-        bot_avatar = "https://cdn-icons-png.flaticon.com/512/809/809957.png"
+        bot_avatar = "https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Bug/3D/bug_3d.png"
     elif alert_type == "rising" or "mới nổi" in channel_name.lower():
         bot_username = "chạy đâu con sâu"
-        bot_avatar = "https://cdn-icons-png.flaticon.com/512/809/809957.png"
+        bot_avatar = "https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Bug/3D/bug_3d.png"
     else:
         bot_username = "Đi đâu con lợn này"
-        bot_avatar = "https://cdn-icons-png.flaticon.com/512/2619/2619277.png"
+        bot_avatar = "https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Pig%20face/3D/pig_face_3d.png"
 
     payload = {
         "username": bot_username,
